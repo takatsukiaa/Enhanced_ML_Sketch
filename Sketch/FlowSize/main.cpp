4ab040ceb101b81c1d1eb238ef9099c98f509dc4
takatsukiaa <89665567k@gmail.com>
2024-11-17 21:29:27 +0800

    added ARE

--- Sketch/FlowSize/main.cpp
+++ Sketch/FlowSize/main.cpp
@@ -30,27 +30,34 @@ int main(){
 	file.open("binary.dat",std::ios::binary);
 	memset(buffer,0,13);
 	FILE* out = fopen("result.txt", "w");
- 	// for (auto it = actual_size.begin(); it != actual_size.end(); ++it) 
-	// {
-    //     Sketch->PrintCounterFile(reinterpret_cast<cuc*>(const_cast<char*>(it->first.c_str())), it->second, out);
-    // }
+	FILE* counters = fopen("counters.txt","w");
+	FILE* all_flows = fopen("flows.txt","w");
+ 	for (auto it = actual_size.begin(); it != actual_size.end(); ++it) 
+	{
+        cuc* temp = reinterpret_cast<cuc*>(const_cast<char*>(it->first.c_str()));
+		Sketch->PrintCounterFile(temp, it->second, all_flows);
+    }
 	cuc* path = (unsigned char*)"parameter.txt";
 	Sketch->LoadPara(path);
 	float AAE = 0;
+	float ARE = 0;
 	while(file.read(reinterpret_cast<char*>(buffer),13)|| file.gcount() > 0)
 	{
 		std::string data(reinterpret_cast<char*>(buffer), 13);
 		cuc* constData = buffer;
-		// AAE+=Sketch->CalculateAAE(constData,actual_size[data]);
 		uint a = actual_size[data];
-		uint query_val = Sketch->Query(constData,true);
-		// printf("Actual Size: %u Query Value: %u\n", a, query_val);
-		// fprintf_s(out,"Actual Size: %u Query Value: %u\n", a, query_val);
+		uint query_val = Sketch->Query(constData,false);
+		AAE+=Sketch->CalculateAAE(constData,actual_size[data]);
+		ARE+=Sketch->CalculateARE(constData,actual_size[data]);
+		fprintf_s(out,"Actual Size: %u Query Value: %u\n", a, query_val);
 		// AAE+=Sketch->CalculateAAE_ML(constData,a,query_val);
-		Sketch->PrintCounterFile(constData,a,out);
+		Sketch->PrintCounterFile(constData,a,counters);
 	}
 	AAE /= actual_size.size();
-	std::cout<<"AAE: "<<AAE;
+	ARE /= actual_size.size();
+	std::cout<<"AAE: "<<AAE<<std::endl;
+	std::cout<<"ARE: "<<ARE<<std::endl;
 	fclose(out);
+	fclose(counters);
 	return 0;
 }
