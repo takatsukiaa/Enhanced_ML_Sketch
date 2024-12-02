af420811c97cd4867e85307a41ee0f9bdc5f0ecb
Sam7202 <nk98048@gmail.com>
2024-12-03 00:06:44 +0800

    add comments

--- Sketch/FlowSize/main.cpp
+++ Sketch/FlowSize/main.cpp
@@ -32,6 +32,8 @@ int main(){
 		packet_count++;
 	}
 	file.close();
+
+
 	file.open(dat_path,std::ios::binary);
 	memset(buffer,0,13);
 	FILE* out = fopen("result.txt", "w");
@@ -39,7 +41,8 @@ int main(){
 	FILE* all_flows = fopen("flows.txt","w");
 	float AAE = 0;
 	float ARE = 0;
-
+	
+	//print all flows to file
 	//for each flows in "actual size" print counter to file, avoid repeated print
  	for (auto it = actual_size.begin(); it != actual_size.end(); ++it) 
 	{
@@ -58,7 +61,7 @@ int main(){
 	//這邊不用print counter，因為會重複print FLOW的counter
 	while(file.read(reinterpret_cast<char*>(buffer),13)|| file.gcount() > 0)
 	{	
-		// packet_num++;
+		
 		std::string data(reinterpret_cast<char*>(buffer), 13);
 		cuc* constData = buffer;
 		ull a = actual_size[data];
@@ -73,7 +76,6 @@ int main(){
 	AAE /= actual_size.size();
 
 	std::cout<<"AAE: "<<AAE<<std::endl;
-	// std::cout<<"AAE per packet: "<<AAE<<std::endl;
 	// std::cout<<"ARE: "<<ARE<<std::endl;
 	fclose(out);
 	fclose(counters);
